dbe1eb21d3a4dd87bc71228d09b7b1eb0307f19a
Julian McNeill <julesdowork@gmail.com>
2018-07-14 17:37:15 -0400

    Add up projected and actual amounts in checking table

--- script.js
+++ script.js
@@ -7,33 +7,73 @@ $(document).ready(function() {
     });
 
     let moneyInputs = getMoneyInputsArray();
-    // console.log(moneyInputs);
-});
 
-function getInitialMonth() {
-    let currentDate = new Date();
-    let monthStr = currentDate.getFullYear() + "-";
-    let currentMonth = currentDate.getMonth() + 1;
-    currentMonth = currentMonth < 10 ? "0" + currentMonth : currentMonth;
-    monthStr += currentMonth;
-    return monthStr;
-}
+    let checkingProjectedTotal = 0;
+    let checkingActualTotal = 0;
+    let $checkingProjectedTotalInput = $("#checking-projected-total");
+    let $checkingActualTotalInput = $("#checking-actual-total");
+    let $projectedMoneyInputs = $(".projected");
+    let $actualMoneyInputs = $(".actual");
 
-function getMoneyInputsArray() {
-    let moneyInputs = document.querySelectorAll(".money-input input");
-    moneyInputs = [].slice.call(moneyInputs);   // convert from NodeList to array
-    moneyInputs.forEach(i => {
-        i.addEventListener("change", function(e) {
-            let inputVal = e.target.value;
-            let patt = /^(([1-9]\d*)?\d)(\.\d{0,2})?$/  // finds dollar amounts
-            if (!patt.test(inputVal)) {
-                // console.log("Invalid input. Please only use numbers and '.'");
-                $(this).addClass("invalid-input");
-            } else {
-                $(this).removeClass("invalid-input");
-            }
-        });
+    $projectedMoneyInputs.change(function(e) {
+        if (!$(this).hasClass("invalid-input")) {
+            checkingProjectedTotal = sumAllInChecking("projected", $projectedMoneyInputs);
+            displayCheckingTotals();
+        }
     });
 
-    return moneyInputs
-}
+    $actualMoneyInputs.change(function(e) {
+        if (!$(this).hasClass("invalid-input")) {
+            checkingActualTotal = sumAllInChecking("actual", $actualMoneyInputs);
+            displayCheckingTotals();
+        }
+    })
+
+    function getInitialMonth() {
+        let currentDate = new Date();
+        let monthStr = currentDate.getFullYear() + "-";
+        let currentMonth = currentDate.getMonth() + 1;
+        currentMonth = currentMonth < 10 ? "0" + currentMonth : currentMonth;
+        monthStr += currentMonth;
+        return monthStr;
+    }
+
+    function getMoneyInputsArray() {
+        let moneyInputs = document.querySelectorAll(".money-input input");
+        moneyInputs = [].slice.call(moneyInputs);   // convert from NodeList to array
+        moneyInputs.forEach(i => {
+            i.addEventListener("change", function(e) {
+                let inputVal = e.target.value;
+                let patt = /^(([1-9]\d*)?\d)(\.\d{0,2})?$/  // finds dollar amounts
+                if (inputVal && !patt.test(inputVal)) {
+                    // console.log("Invalid input. Please only use numbers and '.'");
+                    $(this).addClass("invalid-input");
+                } else {
+                    $(this).removeClass("invalid-input");
+                }
+            });
+        });
+
+        return moneyInputs
+    }
+
+    function sumAllInChecking(label, inputs) {
+        inputArr = [].slice.call(inputs);
+        console.log(inputArr);
+        let inputVals = inputArr.map(input => {
+            return +input.value;    // convert to Number
+        });
+        let sum = inputVals.reduce((acc, next) => {
+            return acc + next; 
+        }, 0);
+        return sum;
+    }
+
+    function displayCheckingTotals() {
+        $checkingProjectedTotalInput.val(checkingProjectedTotal.toFixed(2) || 0);
+        $checkingActualTotalInput.val(checkingActualTotal.toFixed(2) || 0);
+    }
+});
+
+
+
